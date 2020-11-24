0192829542a23d6121393198caef1ca617424ace
Abinavannagendran <abinavan002@gmail.com>
2020-11-24 16:46:48 +0530

    Add project files.

---
+++ Dateandtime/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dateandtimelib;
+
+namespace Dateandtime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt1 = new DateTime(2020, 11, 19).Date;
+            DateTime dt2 = new DateTime(2021, 2, 2).Date;
+            var date =  DateTime.Now;
+            var datee = date.Date;
+            // Font changing
+            
+            var date01 = Class1.Dateconversion(date, "MMMM,dd,yyyy");
+            var date02 = Class1.Dateconversion(date, "dd/MM/yyyy");
+            var date03 = Class1.Dateconversion(date, "dd-MMM-yyyy");
+            var date04 = Class1.Dateconversion(date, "dd-MM-yy");
+            var date05 = date.ToLongDateString();
+            var date06 = date.ToShortDateString();            
+            var date07 = Class1.DateSubtractDay(dt1,dt2);
+           
+
+            DateTime first = new DateTime(2000, 1, 1).Date;
+            Period second = new Period(11, 2, 5, 10, 10, 11);
+            Periodd secondd = new Periodd(11, 2, 5);
+
+            var date08 = Class1.DateAddDayWithTime(first,second);//( DateTime(yyyy,MM, dd),Period(yy, MM, dd, hh, mm, ff));
+            var date09 = Class1.DateAddDayWithoutTime(first, secondd);//( DateTime(yyyy,MM, dd),Period(yy, MM, dd));
+            string date10 = Class1.TimeLine(dt1);//To check the time function like future past present
+
+            Console.WriteLine("Today date as in system: " + datee);
+            Console.WriteLine(date01);
+            Console.WriteLine(date02);
+            Console.WriteLine(date03);
+            Console.WriteLine(date04);
+            Console.WriteLine(date05);
+            Console.WriteLine(date06);
+            Console.WriteLine(date07);
+            Console.WriteLine(date08);
+            Console.WriteLine(date09);
+            Console.WriteLine(date10);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Dateandtimelib/Class1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dateandtimelib
+{
+    
+    public sealed class Period
+    {
+        private readonly int years, months, days, hours, minutes, seconds;
+
+        public Period(int years, int months, int days,
+                      int hours, int minutes, int seconds)
+        {
+            this.years = years;
+            this.months = months;
+            this.days = days;
+            this.hours = hours;
+            this.minutes = minutes;
+            this.seconds = seconds;
+        }
+
+        public static DateTime operator +(DateTime lhs, Period rhs)
+        {
+            
+            return lhs.AddYears(rhs.years)
+                      .AddMonths(rhs.months)
+                      .AddDays(rhs.days)
+                      .AddHours(rhs.hours)
+                      .AddMinutes(rhs.minutes)
+                      .AddSeconds(rhs.seconds);
+        }
+    }
+    public sealed class Periodd
+    {
+        private readonly int years, months, days;
+
+        public Periodd(int years, int months, int days)
+                     
+        {
+            this.years = years;
+            this.months = months;
+            this.days = days;
+            
+        }
+
+        public static DateTime operator +(DateTime lhs, Periodd rhs)
+        { 
+            return lhs.AddYears(rhs.years)
+                      .AddMonths(rhs.months)
+                      .AddDays(rhs.days);
+        }
+    }
+    public static class Class1
+    {
+        public static string Dateconversion(DateTime date, String format) {          
+            return date.ToString(format);
+        }
+        public static double DateSubtractDay(DateTime date1, DateTime date2)
+        {            
+            TimeSpan result = date2- (date1);
+            double NrOfDays = result.TotalDays;
+            return NrOfDays;
+        }
+       
+        public static DateTime DateAddDayWithTime(DateTime first, Period second)
+        {
+            DateTime result = first + second;
+            return result;
+        }
+
+        public static DateTime DateAddDayWithoutTime(DateTime first, Periodd second)
+        {
+            DateTime result = first + second;
+            return result.Date;
+        }
+       public static string TimeLine(DateTime date2)
+        {
+            DateTime date1 = DateTime.Now.Date;
+            if (date1 == date2)
+            {
+                return "istoday";
+            }
+            else if (date1 > date2)
+            {
+
+                return "ispastdate";
+            }
+            else if (date1 < date2)
+            {
+
+                return "isfuturedate";
+            }
+            else { return "Invalid"; }
+
+        }
+        
+        
+    }
+
+
+}
+
